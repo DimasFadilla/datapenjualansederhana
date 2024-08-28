4393085ceb4159500397bdbd2d09b71623591123
DimasFadilla <muhammaddimasfadilla06@gmail.com>
2024-08-28 20:12:05 +0700

    first commot git

---
+++ 13925/Tugas7_A114409_A11202113302/penjualan/bckjual.sql
@@ -0,0 +1,161 @@
+-- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: penjualan
+-- ------------------------------------------------------
+-- Server version	10.4.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barang`
+--
+
+DROP TABLE IF EXISTS `barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barang` (
+  `kd_brg` char(10) NOT NULL,
+  `nm_brg` char(25) DEFAULT NULL,
+  `satuan` char(10) DEFAULT NULL,
+  `harga` double NOT NULL,
+  `stok` int(5) DEFAULT NULL,
+  `stok_min` int(4) NOT NULL,
+  PRIMARY KEY (`kd_brg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barang`
+--
+
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+INSERT INTO `barang` VALUES ('A001','Topi','buah',200000,1198,10),('A002','Sendal','buah',50000,7,10),('A003','Sisir','buah',20000,40,10);
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `djual`
+--
+
+DROP TABLE IF EXISTS `djual`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `djual` (
+  `no_jual` int(10) NOT NULL,
+  `kd_brg` char(6) NOT NULL,
+  `harga_jual` float DEFAULT NULL,
+  `jml_jual` int(4) DEFAULT NULL,
+  PRIMARY KEY (`no_jual`,`kd_brg`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `djual`
+--
+
+LOCK TABLES `djual` WRITE;
+/*!40000 ALTER TABLE `djual` DISABLE KEYS */;
+INSERT INTO `djual` VALUES (10,'A001',200000,1),(10,'A002',50000,5),(11,'A001',200000,3),(11,'A002',50000,10),(12,'A001',200000,30),(15,'A001',200000,10),(17,'A001',200000,2),(17,'A003',20000,10);
+/*!40000 ALTER TABLE `djual` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jual`
+--
+
+DROP TABLE IF EXISTS `jual`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jual` (
+  `no_jual` int(10) NOT NULL,
+  `kd_kons` char(6) DEFAULT NULL,
+  `tgl_jual` date DEFAULT NULL,
+  PRIMARY KEY (`no_jual`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jual`
+--
+
+LOCK TABLES `jual` WRITE;
+/*!40000 ALTER TABLE `jual` DISABLE KEYS */;
+INSERT INTO `jual` VALUES (10,'1','2023-06-25'),(11,'1','2023-06-26'),(12,'1','2023-06-28'),(15,'1','2023-06-28'),(17,'1','2023-06-29');
+/*!40000 ALTER TABLE `jual` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `konsumen`
+--
+
+DROP TABLE IF EXISTS `konsumen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `konsumen` (
+  `kd_kons` varchar(6) NOT NULL,
+  `nm_kons` varchar(30) DEFAULT NULL,
+  `alm_kons` varchar(50) DEFAULT NULL,
+  `kota_kons` varchar(20) DEFAULT NULL,
+  `kd_pos` varchar(5) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`kd_kons`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `konsumen`
+--
+
+LOCK TABLES `konsumen` WRITE;
+/*!40000 ALTER TABLE `konsumen` DISABLE KEYS */;
+INSERT INTO `konsumen` VALUES ('1','abil','merpati raya','semarang','1234','081238212','abil@gmail.com'),('2','arkhan','garuda jaya','jakarta','12390','081239748839','arkhan21@gmail.com');
+/*!40000 ALTER TABLE `konsumen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `password` varchar(254) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'A01','Abil','895387571c9aee012860c1048903880d'),(2,'A02','Arkhan','f4d11d2fc6868861094a0427911fd5e8');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-03 10:50:07
---
+++ 13925/pert13/src/main/java/com/mycompany/pert13/Pert13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert13;
+
+/**
+ *
+ * @author D2A
+ */
+public class Pert13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
